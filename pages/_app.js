401b8ea211d6c2b9ddf54b3e16d287795b740d49
Ivan Draganov <dev.advantech@gmail.com>
2023-06-29 16:24:34 +0300

    add header component

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import 'tailwindcss/tailwind.css'
+import React, { useEffect, useState } from 'react'
+import { Layout } from '@/components'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+
 }
